89fc10c9fdfe68a1a5b8051c8fa4fa19e08cf8b8
Matilda Pan <matilda.yipan@gmail.com>
2018-10-14 22:23:09 +1100

    update create asset

--- services/shoppingcentreAsset.service/src/lib/createAsset.js
+++ services/shoppingcentreAsset.service/src/lib/createAsset.js
@@ -1,26 +1,34 @@
 const Asset = require('../database/models').Asset;
+const ShoppingCentre = require('../database/models').ShoppingCentre;
 let uuid = require('uuid/v4');
 
 const createAsset = async (ctx) => {
-  const combinedParams = Object.assign({}, ctx.params, {id: uuid(), createdAt: new Date(), updatedAt: new Date()} );
-  try {    
-    const data = await Asset.create(combinedParams);
-    const log = {
-      entity: 'asset',
-      event: 'create',
-      entityId: data.id,
-      userId: ctx.meta.reqTokenData.userId,
-      eventTimestamp: new Date(),
-      newData:data,
-      previousData: {}
-    };
-    ctx.call('auditlog.eventLogger', log);
-    return {data};
-  } catch (error) {
-    console.error(error);
-    return {message: 'unable to create an asset...', status: 500};
+  const shoppingCentre = await ShoppingCentre.findOne({where: {id: ctx.params.shoppingCentreId, deletedAt: null}});
+  console.log({shoppingCentre});
+  if(!shoppingCentre){
+    return {message: 'invalid shopping centre id', status: 400};
+  }else{
+    const combinedParams = Object.assign({}, ctx.params, {id: uuid(), createdAt: new Date(), updatedAt: new Date()} );
+    try {    
+      const data = await Asset.create(combinedParams);
+      const log = {
+        entity: 'asset',
+        event: 'create',
+        entityId: data.id,
+        userId: ctx.meta.reqTokenData.userId,
+        eventTimestamp: new Date(),
+        newData:data,
+        previousData: {}
+      };
+      ctx.call('auditlog.eventLogger', log);
+      return {data};
+    } catch (error) {
+      console.error(error);
+      return {message: 'unable to create an asset...', status: 500};
+    }
+  
   }
-
 };
+  
 
 module.exports = createAsset;
